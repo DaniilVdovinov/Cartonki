68069276c5996f8a8e34f2220041eecd73f69b74
kami_khai <kamilla-h@bk.ru>
2019-04-18 10:20:19 +0300

    add list with cards

--- Cartonki/app/src/main/java/f/cartonki/DecksActivity.java
+++ Cartonki/app/src/main/java/f/cartonki/DecksActivity.java
@@ -1,26 +1,47 @@
 package f.cartonki;
 
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.ResolveInfo;
 import android.content.res.Configuration;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.TypedValue;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.*;
+import com.baoyz.swipemenulistview.SwipeMenu;
+import com.baoyz.swipemenulistview.SwipeMenuCreator;
+import com.baoyz.swipemenulistview.SwipeMenuItem;
+import com.baoyz.swipemenulistview.SwipeMenuListView;
+import f.models.Card;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DecksActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     DrawerLayout drawer;
     ActionBarDrawerToggle toggle;
+
+    private List<String> mAppList;
+    private AppAdapter mAdapter;
+    private SwipeMenuListView mListView;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,6 +69,115 @@ public class DecksActivity extends AppCompatActivity implements NavigationView.O
         NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view);
         navigationView.setNavigationItemSelectedListener(this);
         navigationView.bringToFront();
+
+//        DBHelper dbHelper = new DBHelper();
+
+//        mAppList = dbHelper.getCardsRepository().findAll();
+        mAppList = new ArrayList<>();
+        mAppList.add("Раз колода");
+        mAppList.add("Два колода");
+        mAppList.add("Три колода");
+        mAppList.add("Три колода");
+        mAppList.add("Три колода");
+        mAppList.add("Три колода");
+        mAppList.add("Три колода");
+        mAppList.add("Три колода");
+        mAppList.add("Три колода");
+        mAppList.add("Три колода");
+        mAppList.add("Три колода");
+        mAppList.add("Три колода");
+
+        mListView = (SwipeMenuListView) findViewById(R.id.listView_decks_activity);
+
+        mAdapter = new AppAdapter();
+        mListView.setAdapter(mAdapter);
+
+        // step 1. create a MenuCreator
+        SwipeMenuCreator creator = new SwipeMenuCreator() {
+
+            @Override
+            public void create(SwipeMenu menu) {
+
+                //кнопка редактирования
+                SwipeMenuItem editItem = new SwipeMenuItem(
+                        getApplicationContext());
+                editItem.setBackground(new ColorDrawable(Color.rgb(0xC9, 0xC9,
+                        0xCE)));
+                editItem.setWidth(dp2px(90));
+                editItem.setIcon(R.drawable.edit);
+                menu.addMenuItem(editItem);
+
+                //кнопка удаления
+                SwipeMenuItem deleteItem = new SwipeMenuItem(
+                        getApplicationContext());
+                deleteItem.setBackground(new ColorDrawable(Color.rgb(0xF9,
+                        0x3F, 0x25)));
+                deleteItem.setWidth(dp2px(90));
+                deleteItem.setIcon(R.drawable.delete);
+                menu.addMenuItem(deleteItem);
+            }
+        };
+        mListView.setMenuCreator(creator);
+
+        // step 2. listener item click event
+        mListView.setOnMenuItemClickListener(new SwipeMenuListView.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(int position, SwipeMenu menu, int index) {
+//                ApplicationInfo item = mAppList.get(position);
+                switch (index) {
+                    case 0:
+//                        edit(item);
+                        break;
+                    case 1:
+                        // delete
+//					delete(item);
+                        mAppList.remove(position);
+                        mAdapter.notifyDataSetChanged();
+                        break;
+                }
+                return false;
+            }
+        });
+
+        // set SwipeListener
+        mListView.setOnSwipeListener(new SwipeMenuListView.OnSwipeListener() {
+
+            @Override
+            public void onSwipeStart(int position) {
+                // swipe start
+            }
+
+            @Override
+            public void onSwipeEnd(int position) {
+                // swipe end
+            }
+        });
+
+        // set MenuStateChangeListener
+        mListView.setOnMenuStateChangeListener(new SwipeMenuListView.OnMenuStateChangeListener() {
+            @Override
+            public void onMenuOpen(int position) {
+            }
+
+            @Override
+            public void onMenuClose(int position) {
+            }
+        });
+
+        // other setting
+//		listView.setCloseInterpolator(new BounceInterpolator());
+
+        // test item long click
+        mListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view,
+                                           int position, long id) {
+                Toast.makeText(getApplicationContext(), position + " long click", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+
     }
 
     @Override
@@ -115,4 +245,104 @@ public class DecksActivity extends AppCompatActivity implements NavigationView.O
         super.onConfigurationChanged(newConfig);
         toggle.onConfigurationChanged(newConfig);
     }
+
+    private void delete(ApplicationInfo item) {
+        // delete app
+        try {
+            Intent intent = new Intent(Intent.ACTION_DELETE);
+            intent.setData(Uri.fromParts("package", item.packageName, null));
+            startActivity(intent);
+        } catch (Exception e) {
+        }
+    }
+
+    private void edit(ApplicationInfo item) {
+        // edit app
+        Intent resolveIntent = new Intent(Intent.ACTION_MAIN, null);
+        resolveIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+        resolveIntent.setPackage(item.packageName);
+        List<ResolveInfo> resolveInfoList = getPackageManager()
+                .queryIntentActivities(resolveIntent, 0);
+        if (resolveInfoList != null && resolveInfoList.size() > 0) {
+            ResolveInfo resolveInfo = resolveInfoList.get(0);
+            String activityPackageName = resolveInfo.activityInfo.packageName;
+            String className = resolveInfo.activityInfo.name;
+
+            Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.addCategory(Intent.CATEGORY_LAUNCHER);
+            ComponentName componentName = new ComponentName(
+                    activityPackageName, className);
+
+            intent.setComponent(componentName);
+            startActivity(intent);
+        }
+    }
+
+    class AppAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return mAppList.size();
+        }
+
+        @Override
+        public String getItem(int position) {
+            return mAppList.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = View.inflate(getApplicationContext(),
+                        R.layout.item_list_app, null);
+                new ViewHolder(convertView);
+            }
+            ViewHolder holder = (ViewHolder) convertView.getTag();
+            String item = getItem(position);
+//            holder.iv_icon.setImageDrawable(item.loadIcon(getPackageManager()));
+            holder.tv_name.setText(item);
+//            holder.iv_icon.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//                    Toast.makeText(DecksActivity.this, "iv_icon_click", Toast.LENGTH_SHORT).show();
+//                }
+//            });
+            holder.tv_name.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(DecksActivity.this,"iv_icon_click", Toast.LENGTH_SHORT).show();
+                }
+            });
+            return convertView;
+        }
+
+        class ViewHolder {
+            TextView tv_name;
+
+            public ViewHolder(View view) {
+                tv_name = (TextView) view.findViewById(R.id.tv_name);
+                view.setTag(this);
+            }
+        }
+
+//        @Override
+//        public boolean getSwipEnableByPosition(int position) {
+//            if(position % 2 == 0){
+//                return false;
+//            }
+//            return true;
+//        }
+    }
+
+    private int dp2px(int dp) {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp,
+                getResources().getDisplayMetrics());
+    }
+
+
 }
